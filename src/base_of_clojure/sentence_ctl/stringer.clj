480125078544e637cfdaddc749f03737af2d5d67
behike56 <living.tech.dancing.code@gmail.com>
2024-07-27 10:05:50 +0900

    (comment)の追加。

--- src/base_of_clojure/sentence_ctl/stringer.clj
+++ src/base_of_clojure/sentence_ctl/stringer.clj
@@ -1,5 +1,6 @@
 (ns base-of-clojure.sentence-ctl.stringer)
 (comment
+  "文字列の切り取り"
   (def input_str "36")
 
   (subs input_str 0 1)
@@ -8,4 +9,16 @@
   ;; => "6"
   )
 
+
+(comment
+  "文字列の結合"
+  (def input_strA "abc")
+  (def input_strB "123")
+
+  (str input_strA input_strB)
+  ;; => "3"
+  (subs input_str 1)
+  ;; => "6"
+  )
+(str "abc" "123")
 
