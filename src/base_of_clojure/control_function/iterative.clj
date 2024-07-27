1b28637abfd196788acb5fb7f60d6dea720b49f9
behike56 <living.tech.dancing.code@gmail.com>
2024-07-27 20:46:14 +0900

    繰り返し処理の追加。

--- src/base_of_clojure/control_function/iterative.clj
+++ src/base_of_clojure/control_function/iterative.clj
@@ -1,2 +1,21 @@
 (ns base-of-clojure.control-function.iterative)
 
+(loop [i 0]
+  false
+  (recur (inc i)))
+
+(loop [i 0]
+  (when (< i 5)
+    (recur (inc i))))
+
+(for [i (range 5)]
+  (println i))
+
+(doseq [i (range 5)]
+  (println i))
+
+(doseq [i (take-while #(< % 5) (range 10))]
+  (println i))
+
+(doseq [i (drop-while #(< % 5) (range 10))]
+  (println i))
