20e7d886e5420182332f02d21a070a33ae20a91f
behike56 <living.tech.dancing.code@gmail.com>
2024-09-16 09:10:10 +0900

    リスト、マップ、セットの追加。

--- src/base_of_clojure/sequences/seq_practice.clj
+++ src/base_of_clojure/sequences/seq_practice.clj
@@ -3,14 +3,27 @@
 
 (def vec_a [2 4 6 7 10 13 17])
 (def vec_b [1 3 5 8 9 11 12 14 15 16])
+(def list_a '(1 2 3 4 5))
+(def map_a {:name "Alpha" :age 33 :gender "male"})
+(def set_a #{10 20 30 40 50})
+
 (first vec_a)
 ;; => 2
+(first list_a)
+(first map_a)
+(first set_a)
 (first [])
 ;; => nil
 
 (rest vec_a)
 ;; => (4 6 7 10 13 17)
 (rest [])
+;; => ()
+(rest '())
+;; => ()
+(rest {})
+;; => ()
+(rest #{})
 ;; => ()
 
 (cons 1 vec_b)
@@ -29,6 +42,10 @@
 ;; => nil
 (next '())
 ;; => nil
-(next ("a" "b"))
+(next {})
+;; => nil
+(next #{})
 ;; => nil
 
+(next '("a" "b"))
+;; => ("b")
