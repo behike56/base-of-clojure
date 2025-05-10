40c9ee09c67da5011b9cc0a3d5d5dd85592ff5ed
behike56 <engineering@lambda-publishing.dev>
2025-05-10 19:16:57 +0900

    Add file reader.

--- src/base_of_clojure/core.clj
+++ src/base_of_clojure/core.clj
@@ -1,11 +1,20 @@
 (ns base-of-clojure.core
-  (:require base-of-clojure.filecontroller.file-reader :as fr))
-
-(defn add [a b]
-  (+ a b))
+  (:require [base-of-clojure.filecontroller.file-reader :as fr]
+            [base-of-clojure.filecontroller.file-writer :as fw]
+            [clojure.java.io :as io]))  ;; io をインポート
 
 (defn -main
-  [& args]
-  (fr/file_reader (first args))
-  
-  (add 1 4))
+  []
+  (let [input-resource (io/resource "input/input.csv")  ;; resources/input/input.csv を指定
+        output-file "resources/output/output.csv"  ;; 出力先を resources/output/output.csv にする場合
+        csv-data (fr/read-csv input-resource)
+        data-maps (fr/csv-to-maps csv-data)
+        processed-data (fr/process-data data-maps)  ;; 処理関数を適用
+        _ (fw/maps-to-csv processed-data output-file)]
+    (println "CSVデータ:")
+    (println csv-data)
+    (println "マップ形式のデータ:")
+    (println data-maps)
+    (println "処理後のデータ:")
+    (println processed-data))
+  (println "CSVファイルの処理が完了しました。"))
