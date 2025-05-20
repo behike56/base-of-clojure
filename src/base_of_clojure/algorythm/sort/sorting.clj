bea999ab73ab16ff0de025b9f3059f960f4fd36a
behike56 <engineering@lambda-publishing.dev>
2025-05-20 22:31:20 +0900

    Tiny fix.

--- src/base_of_clojure/algorythm/sort/sorting.clj
+++ src/base_of_clojure/algorythm/sort/sorting.clj
@@ -1,17 +1,11 @@
 (ns base-of-clojure.algorythm.sort.sorting)
 
-(defn bigSorting 
+(defn bigSorting
   "1 ~ 10^6の値を想定。
    文字列として比較を行う。
    桁数の比較を行い、同じ桁数の場合、`compare`関数を使用。
    "
   [unsorted]
-  (sort
-   (fn [a b]
-     (let [len-a (count a)
-           len-b (count b)]
-       (cond
-         (< len-a len-b) -1
-         (> len-a len-b) 1
-         :else (compare a b))))
+  (sort-by
+   (juxt (comp count str) str)
    unsorted))
